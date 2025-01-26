e405e1d59e677240d3aa024b0c3e6a9640262da9
Amrryzen <alvinnoval001@gmail.com>
2025-01-26 23:21:16 +0700

    update3

--- saklar.js
+++ saklar.js
@@ -2,10 +2,27 @@ function saklar() {
     let toggle1 = document.getElementById("default-toggle1");
     let toggle2 = document.getElementById("default-toggle2");
     let toggle3 = document.getElementById("default-toggle3");
+    let toggle4 = document.getElementById("default-toggle4");
+    let toggle5 = document.getElementById("default-toggle5");
+    let toggle6 = document.getElementById("default-toggle6");
+    let toggle7 = document.getElementById("default-toggle7");
+    let toggle8 = document.getElementById("default-toggle8");
+    let toggle9 = document.getElementById("default-toggle9");
+    let toggle10 = document.getElementById("default-toggle10");
+    let toggle11 = document.getElementById("default-toggle11");
+    let toggle12 = document.getElementById("default-toggle12");
+    let toggle13 = document.getElementById("default-toggle13");
+    let toggle14 = document.getElementById("default-toggle14");
 
     let Lampu1 = document.getElementById("Lampu1");
     let Lampu2 = document.getElementById("Lampu2");
     let Lampu3 = document.getElementById("Lampu3");
+    let Lampu4 = document.getElementById("Lampu4");
+    let Lampu5 = document.getElementById("Lampu5");
+    let Lampu6 = document.getElementById("Lampu6");
+    let Lampu7 = document.getElementById("Lampu7");
+    let Lampu8 = document.getElementById("Lampu8");
+    let Lampu9 = document.getElementById("Lampu9");
 
     if (toggle1.checked) {
         Lampu1.src = "Assets/Images/on.gif";
@@ -23,5 +40,70 @@ function saklar() {
         Lampu3.src = "Assets/Images/on.gif";
     } else {
         Lampu3.src = "Assets/Images/off.gif";
+    }
+
+    if (toggle4.checked) {
+        Lampu4.src = "Assets/Images/on.gif";
+    } else {
+        Lampu4.src = "Assets/Images/off.gif";
+    }
+
+    if (toggle5.checked) {
+        Lampu5.src = "Assets/Images/on.gif";
+    } else {
+        Lampu5.src = "Assets/Images/off.gif";
+    }
+
+    if (toggle6.checked) {
+        Lampu6.src = "Assets/Images/on.gif";
+    } else {
+        Lampu6.src = "Assets/Images/off.gif";
+    }
+
+    if (toggle7.checked) {
+        Lampu7.src = "Assets/Images/on.gif";
+    } else {
+        Lampu7.src = "Assets/Images/off.gif";
+    }
+
+    if (toggle8.checked) {
+        Lampu8.src = "Assets/Images/on.gif";
+    } else {
+        Lampu8.src = "Assets/Images/off.gif";
+    }
+
+    if (toggle9.checked) {
+        Lampu9.src = "Assets/Images/on.gif";
+    } else {
+        Lampu9.src = "Assets/Images/off.gif";
+    }
+
+    if (toggle10.checked) {
+        Lampu10.src = "Assets/Images/on.gif";
+    } else {
+        Lampu10.src = "Assets/Images/off.gif";
+    }
+
+    if (toggle11.checked) {
+        Lampu1.src = "Assets/Images/on.gif";
+        Lampu2.src = "Assets/Images/on.gif";
+        Lampu3.src = "Assets/Images/on.gif";
+    }
+
+    if (toggle12.checked) {
+        Lampu4.src = "Assets/Images/on.gif";
+    }
+
+    if (toggle13.checked) {
+        Lampu5.src = "Assets/Images/on.gif";
+        Lampu6.src = "Assets/Images/on.gif";
+
+    }
+
+    if (toggle14.checked) {
+        Lampu7.src = "Assets/Images/on.gif";
+        Lampu8.src = "Assets/Images/on.gif";
+        Lampu9.src = "Assets/Images/on.gif";
+        Lampu10.src = "Assets/Images/on.gif";
     }
 }
